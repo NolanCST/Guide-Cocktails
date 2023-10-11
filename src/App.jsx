f5fd8b394119d9c230d893c72fe60c872562c6db
NolanCST <nolan.costa06@gmail.com>
2023-10-11 13:35:11 +0200

    CSS en cours

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,82 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import RenderCoctail from "./components/RenderCoctail";
+import { Box, Button, Grid, TextField } from "@mui/material";
 
 function App() {
    const [cocktailArray, setCocktailArray] = useState([]);
+   const [search, setSearch] = useState("");
 
-   const getCocktail = async () => {
-      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`);
-      const data = await response.json();
-      setCocktailArray(data.drinks);
-      console.log(cocktailArray);
+   const handleInputChange = (e) => {
+      setSearch(e.target.value);
    };
 
-   useEffect(() => {
-      getCocktail();
-   }, []);
+   const getCocktail = async () => {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`);
+      const data = await response.json();
+      setCocktailArray(data.drinks);
+   };
 
    const renderCocktailArray = () => {
       return cocktailArray?.map((element, index) => {
-         return <RenderCoctail />;
+         return (
+            <Grid item display="flex" alignItems="center" justifyContent="center" sm={5} key={index} mt={10}>
+               <RenderCoctail
+                  title={element.strDrink}
+                  image={element.strDrinkThumb}
+                  instruction={element.strInstructions}
+                  ingredients1={element.strIngredient1}
+                  ingredients2={element.strIngredient2}
+                  ingredients3={element.strIngredient3}
+                  ingredients4={element.strIngredient4}
+                  ingredients5={element.strIngredient5}
+                  ingredients6={element.strIngredient6}
+                  ingredients7={element.strIngredient7}
+                  ingredients8={element.strIngredient8}
+                  ingredients9={element.strIngredient9}
+                  ingredients10={element.strIngredient10}
+                  ingredients11={element.strIngredient11}
+                  ingredients12={element.strIngredient12}
+                  ingredients13={element.strIngredient13}
+                  ingredients14={element.strIngredient14}
+                  ingredients15={element.strIngredient15}
+                  mesure1={element.strMeasure1}
+                  mesure2={element.strMeasure2}
+                  mesure3={element.strMeasure3}
+                  mesure4={element.strMeasure4}
+                  mesure5={element.strMeasure5}
+                  mesure6={element.strMeasure6}
+                  mesure7={element.strMeasure7}
+                  mesure8={element.strMeasure8}
+                  mesure9={element.strMeasure9}
+                  mesure10={element.strMeasure10}
+                  mesure11={element.strMeasure11}
+                  mesure12={element.strMeasure12}
+                  mesure13={element.strMeasure13}
+                  mesure14={element.strMeasure14}
+                  mesure15={element.strMeasure15}
+               />
+            </Grid>
+         );
       });
    };
 
-   console.log(cocktailArray);
-
    return (
       <>
-         <div></div>
+         <Box display="flex" flexDirection="column" alignItems="center">
+            <div className="baniere">
+               <h1>Les cocktails de Nono</h1>
+               <div>
+                  <TextField onChange={handleInputChange} id="outlined-basic" label="Quelle recette voulez-vous ?" variant="outlined" />
+                  <Button onClick={getCocktail} variant="outlined">
+                     Rechercher
+                  </Button>
+               </div>
+            </div>
+            <Grid container justifyContent="center">
+               {renderCocktailArray()}
+            </Grid>
+         </Box>
       </>
    );
 }
--- src/components/RenderCoctail.jsx
+++ src/components/RenderCoctail.jsx
@@ -1,4 +1,179 @@
-function RenderCoctail() {
-   return;
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+// import FavoriteIcon from "@mui/icons-material/Favorite";
+// import ShareIcon from "@mui/icons-material/Share";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+// import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const ExpandMore = styled((props) => {
+   const { expand, ...other } = props;
+   return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+   marginLeft: "auto",
+   transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+   }),
+}));
+
+function RenderCoctail(props) {
+   const [expanded, setExpanded] = React.useState(false);
+
+   const handleExpandClick = () => {
+      setExpanded(!expanded);
+   };
+   return (
+      <Card sx={{ maxWidth: 345 }}>
+         <CardHeader
+            avatar={
+               <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  N
+               </Avatar>
+            }
+            // action={
+            //    <IconButton aria-label="settings">
+            //       <MoreVertIcon />
+            //    </IconButton>
+            // }
+            title={props.title}
+         />
+         <CardMedia component="img" height="194" image={props.image} alt="image cocktail" />
+         <CardActions disableSpacing>
+            {/* <IconButton aria-label="add to favorites">
+               <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+               <ShareIcon />
+            </IconButton>
+            <ExpandMore expand={expanded} onClick={handleExpandClick} aria-expanded={expanded} aria-label="show more">
+               <ExpandMoreIcon />
+            </ExpandMore> */}
+         </CardActions>
+         {/* <Collapse in={expanded} timeout="auto" unmountOnExit> */}
+         <CardContent>
+            <Typography paragraph>Ingredients:</Typography>
+            <Typography>
+               {props.ingredients1 || props.mesure1 ? (
+                  <li>
+                     {props.ingredients1} {props.mesure1}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients2 || props.mesure2 ? (
+                  <li>
+                     {props.ingredients2} {props.mesure2}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients3 || props.mesure3 ? (
+                  <li>
+                     {props.ingredients3} {props.mesure3}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients4 || props.mesure4 ? (
+                  <li>
+                     {props.ingredients4} {props.mesure4}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients5 || props.mesure5 ? (
+                  <li>
+                     {props.ingredients5} {props.mesure5}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients6 || props.mesure6 ? (
+                  <li>
+                     {props.ingredients6} {props.mesure6}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients7 || props.mesure7 ? (
+                  <li>
+                     {props.ingredients7} {props.mesure7}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients8 || props.mesure8 ? (
+                  <li>
+                     {props.ingredients8} {props.mesure8}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients9 || props.mesure9 ? (
+                  <li>
+                     {props.ingredients9} {props.mesure9}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients10 || props.mesure10 ? (
+                  <li>
+                     {props.ingredients10} {props.mesure10}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients11 || props.mesure11 ? (
+                  <li>
+                     {props.ingredients11} {props.mesure11}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients12 || props.mesure12 ? (
+                  <li>
+                     {props.ingredients12} {props.mesure12}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients13 || props.mesure13 ? (
+                  <li>
+                     {props.ingredients13} {props.mesure13}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients14 || props.mesure14 ? (
+                  <li>
+                     {props.ingredients14} {props.mesure14}
+                  </li>
+               ) : (
+                  ``
+               )}
+               {props.ingredients15 || props.mesure15 ? (
+                  <li>
+                     {props.ingredients15} {props.mesure15}
+                  </li>
+               ) : (
+                  ``
+               )}
+            </Typography>
+            <Typography paragraph>Method:</Typography>
+            <Typography>{props.instruction}</Typography>
+         </CardContent>
+         {/* </Collapse> */}
+      </Card>
+   );
 }
 export default RenderCoctail;
